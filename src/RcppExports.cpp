03a6af9e0d874852f8f7225fe0b6b9797a8c43ca
David Holstius <david.holstius@gmail.com>
2018-04-25 14:22:59 -0700

    initial commit

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -9,15 +9,11 @@ using namespace Rcpp;
 IntegerVector fmatch(std::vector< std::string > x, std::vector< std::string > y);
 RcppExport SEXP fmatch_fmatch(SEXP xSEXP, SEXP ySEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< std::vector< std::string > >::type x(xSEXP );
-        Rcpp::traits::input_parameter< std::vector< std::string > >::type y(ySEXP );
-        IntegerVector __result = fmatch(x, y);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< std::vector< std::string > >::type x(xSEXP);
+    Rcpp::traits::input_parameter< std::vector< std::string > >::type y(ySEXP);
+    __result = Rcpp::wrap(fmatch(x, y));
+    return __result;
 END_RCPP
 }
