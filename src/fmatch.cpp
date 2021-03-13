f3d2b4f97153757670944cbb377073d6e1c5a9e6
David Holstius <david.holstius@gmail.com>
2021-03-12 18:54:52 -0800

    remove superfluous export

--- src/fmatch.cpp
+++ src/fmatch.cpp
@@ -24,8 +24,6 @@ char *as_c_str( String s )
 //' @param y character
 //' @return integer positions of matches in \code{y} (\code{NA} indicates no match)
 //' 
-//' @export
-//' 
 // [[Rcpp::export]]
 IntegerVector fmatch( CharacterVector x, CharacterVector y ) 
 {
